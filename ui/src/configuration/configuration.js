45652a596b3a03aa8be28ea1ae4f6e69ed2eaea2
Kirti Rohira <rohirakirti@gmail.com>
2020-08-17 01:46:22 +0530

    set configuration file and dashboard

---
+++ ui/src/configuration/configuration.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4K-ENdlSk7bJ38wwkUYvFVfLHsdpm_4s",
+    authDomain: "summer20-sps-86.firebaseapp.com",
+    databaseURL: "https://summer20-sps-86.firebaseio.com",
+    projectId: "summer20-sps-86",
+    storageBucket: "summer20-sps-86.appspot.com",
+    messagingSenderId: "262008738474",
+    appId: "1:262008738474:web:1c7923c002007145ba36d3"
+};
+
+export default {
+    routes: {
+        questions : 'https://asia-east2-summer20-sps-86.cloudfunctions.net/questions/',
+        answers: 'https://asia-east2-summer20-sps-86.cloudfunctions.net/answers/',
+    }
+}
